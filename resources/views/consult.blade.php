89f0e71a0553d9efd583512101a460466f3361f3
lamsri1992 <satiya3577@gmail.com>
2023-06-07 10:53:05 +0700

    update api url

--- resources/views/consult.blade.php
+++ resources/views/consult.blade.php
@@ -107,7 +107,7 @@
             if (result.dismiss === Swal.DismissReason.timer) {}
         })
         $.ajax({
-            url: "api/visit/" + vn,
+            url: "wdhp-consult/api/visit/" + vn,
             success: function (result) {
                 document.getElementById("txtnote").hidden = false;
                 document.getElementById("txtlevel").hidden = false;
@@ -206,7 +206,7 @@
         });
 
         $.ajax({
-            url: "/api/diag/" + vn,
+            url: "wdhp-consult/api/diag/" + vn,
             success: function (data) {
                 document.getElementById("dx").hidden = false;
                 $("#dx tbody").html("");
@@ -231,7 +231,7 @@
         });
 
         $.ajax({
-            url: "/api/drug/" + vn,
+            url: "wdhp-consult/api/drug/" + vn,
             success: function (data) {
                 document.getElementById("drug").hidden = false;
                 $("#drug tbody").html("");
